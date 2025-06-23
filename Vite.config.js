82918fc142a5e10e824740e5b9f337769657de44
bagrechajayesh <jayeshbagrecha@gmail.com>
2025-06-23 13:07:53 +0530

    Updated Vite

--- Vite.config.js
+++ Vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
+  plugins: [react()]
 })
