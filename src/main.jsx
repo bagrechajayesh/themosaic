a729e3b678fc82628ceb5c5d6e5f2aac0b83194a
bagrechajayesh <jayeshbagrecha@gmail.com>
2025-06-23 12:09:11 +0530

    Trying the new code

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './pages/App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const Main = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-E5WSY9HJQM';
-    script.async = true;
-    document.head.appendChild(script);
-
-    const inlineScript = document.createElement('script');
-    inlineScript.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-E5WSY9HJQM');
-    `;
-    document.head.appendChild(inlineScript);
-  }, []);
-
-  return <App />;
-};
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
